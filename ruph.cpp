d04497072dc3f191ba6f9b8797ebf35078438f04
nit sharma <nit8339@gmail.com>
2021-09-09 07:53:35 +0530

    9sept

--- ruph.cpp
+++ ruph.cpp
@@ -16,54 +16,11 @@ void printmap(map<ll,ll> mp){for(auto val:mp)cout<<val.first<<" "<<val.second<<e
 void printvec(vector<ll> vec){for(auto val:vec)cout<<val<<" ";cout<<endl;}
 void printset(set<ll> st){for(auto val:st)cout<<val<<" ";cout<<endl;}
 /*-------------------------------------------------------------------------------*/
-void merge(ll *arr,ll st,ll end){
-   
-    ll mid = (((st+end)/2)+1);
-     ll i = st,j = mid;
-     ll output[end-st+1];
-     ll k=0;
-    while(i<mid&&j<=end){
-        if(arr[i]<arr[j]){
-            output[k++] = arr[i++];
-        }
-        else{
-            output[k++] = arr[j++];
-        }
-    }
-    while(i<mid){
-        output[k++] = arr[i++];
-    }
-    while(j<=end){
-        output[k++] = arr[j++];
-    }
-    k=0;
-    for(int i=st;i<=end;i++){
-       arr[i]=output[k++];
-    }
-    
-
-}
-void merge_sort(ll arr[],ll st,ll end){
-    if(st>=end) return ;
-    ll mid = (st+end)/2;
-    merge_sort(arr,st,mid);
-    merge_sort(arr,mid+1,end);
-    merge(arr,st,end);
-}
-
-
-
 int main(){
 #ifndef ONLINE_JUDGE
 freopen("ipt.txt","r",stdin);
 freopen("out.txt","w",stdout);
 #endif
-    cin>>n;
-    ll arr[n];
-    nit(i,n){
-        cin>>arr[i];
-    }
-    merge_sort(arr,0,n);
-    printarr(arr,n);
+    cout<<2;
 return 0;
 }
